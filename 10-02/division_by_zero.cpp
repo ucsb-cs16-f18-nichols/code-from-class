a6793c2871345a97b146df8683e48d8dad16b1a7
Lawton Nichols <lawtonnichols@gmail.com>
2018-10-02 15:02:12 -0700

    added division by zero example

---
+++ 10-02/division_by_zero.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << 1 / x << endl;
+    return 0;
+}
